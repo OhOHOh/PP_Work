6e10b3fe690ccd9b8edf09f2a501dfd33249ade0
Yu <runyu@paypal.com>
2019-08-11 11:16:39 +0800

    little change

--- Innovation Lab/NLP/get_sentiment_score.py
+++ Innovation Lab/NLP/get_sentiment_score.py
@@ -12,6 +12,7 @@ print(comments.columns)
 # 计算2个 sentiment score: tilte & content
 comments['title_sen_score'] = np.nan
 comments['content_sen_score'] = np.nan
+comments['final_sen_score'] = np.nan
 
 for i in range(comments.shape[0]):
     try:
@@ -22,6 +23,11 @@ for i in range(comments.shape[0]):
         comments.loc[i, 'content_sen_score'] = TextBlob(comments.loc[i, 'content']).sentiment.polarity
     except Exception as e:
         print("contnet error: ".format(e))
+    try:
+        comments.loc[i, 'final_sen_score'] = 0.4*comments.loc[i, 'title_sen_score'] + 0.6*comments.loc[i, 'content_sen_score']
+    except Exception as e:
+        print("final error: {}".format(e))
     print("{:10} comments finished!".format(i))
 
-comments.to_csv("../../../Innovation Lab/reviews_data_tripadv_sen_score.csv", index=False)
+
+comments.to_csv("../../../Innovation Lab/reviews_data_tripadv_sen_score_change_sep.csv", index=False, sep='|')
